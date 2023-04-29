0b8e7eeac76119bd5108f3d3393340077447fa85
vishalsv2 <vishalsv2002@gmail.com>
2023-04-29 10:09:12 +0530

    First Commit

--- Main_Ceasar Cipher.py
+++ Main_Ceasar Cipher.py
@@ -1,13 +1,14 @@
 from Logo import logo
 print (logo)
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','1','2','3','4','5','6','7','8','9','0','!','@','#','$','%','^','&','*','(',')','-','+','_','=','{','}','[',']','"\"','|',';','"',"'","<",">",",",".","/","?"]
 
 should_end = True
 while should_end:
     direction = input("Type 'encrypt' for encode and 'decrypt' for decode \n").lower()
     text=input("Type the word  : ")
     shift=int(input("Type the shift key : "))
+    
     def ceasar(text,shift,direction):
         if direction=="encrypt":
             cipher_text=""
